ef39b63cf927e7bf0fde8aea857bce3d5128f708
AethiQs-Max <max.van.den.hoven@aethiqs.nl>
2022-06-18 13:20:12 +0200

    Update processor.py

--- continuous_environment/processor.py
+++ continuous_environment/processor.py
@@ -2,6 +2,7 @@ from PIL import Image
 import numpy as np
 from rl.core import Processor
 
+
 INPUT_SHAPE = (84, 84, 1)
 INPUT_SHAPE_FIXED = (84, 84)
 INPUT_SHAPE_BATCH = (1, 84, 84, 1)
@@ -11,13 +12,12 @@ WINDOW_LENGTH = 4
 class RoombaProcessor(Processor):
     def process_observation(self, observation):
         assert observation.ndim == 3  # (height, width, channel)
-       # print(observation)
         img = Image.fromarray(observation)
         img = img.resize(INPUT_SHAPE_FIXED).convert('L')  # resize and convert to grayscale
         processed_observation = np.array(img).reshape(INPUT_SHAPE_BATCH)
-       # print(processed_observation.reshape((INPUT_SHAPE_BATCH)))
         assert processed_observation.shape == INPUT_SHAPE_BATCH
         return processed_observation.astype('uint8')  # saves storage in experience memory
+
 
     def process_state_batch(self, batch):
         # We could perform this processing step in `process_observation`. In this case, however,
@@ -25,6 +25,7 @@ class RoombaProcessor(Processor):
         # an `uint8` array. This matters if we store 1M observations.
         processed_batch = batch.astype('float32') / 255.
         return processed_batch
+
 
     def process_reward(self, reward):
         #return np.clip(reward, -1., 1.)
